4500a0f7a13e3240cd9c366cce1331ff8c4a6c0e
Tharangaj <nteeje@gmail.com>
2023-12-18 03:06:15 +0400

    implement base feature

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var map = L.map('map', {
     attributionControl: false
 });
 var nzoom = 12;
-  
+
 
 L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}', {
     maxZoom: 20,
@@ -18,9 +18,9 @@ L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}', {
 var postcode = L.icon({
     iconUrl: './favicon.ico',
     iconSize: [50, 50]
-    });
+});
 var marker = L.marker([6.8711724, 79.9237776], {
-    icon:postcode,
+    icon: postcode,
     draggable: true
 }).addTo(map);
 marker.on('dragend', function (e) {
@@ -39,7 +39,7 @@ marker.on('dragend', function (e) {
 
 });
 
-function setMarker(latLng,zoom) {
+function setMarker(latLng, zoom) {
     // Split the string into an array using the comma as a separator
     const [latitude, longitude] = latLng.split(',');
 
@@ -55,10 +55,10 @@ function getAddressFromCoordinates(lat, lng, marker) {
     fetch("https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=" + lat + "&lon=" + lng)
         .then((response) => response.json())
         .then((json) => {
-           // console.log(json.results[0].formatted_address);
-            marker.bindPopup("Lat " + lat + "<br />Lon " + lng + "</br>address " + json.display_name).openPopup();
-           // marker.bindPopup("Lat " + lat + "<br />Lon " + lng + "</br>address " + json.results[0].formatted_address).openPopup();
-           
+            // console.log(json.results[0].formatted_address);
+            marker.bindPopup(json.display_name).openPopup();
+            // marker.bindPopup("Lat " + lat + "<br />Lon " + lng + "</br>address " + json.results[0].formatted_address).openPopup();
+            document.getElementById('address').value = json.display_name;
         })
         .catch(error => {
             console.error('Error fetching address:', error);
@@ -99,6 +99,10 @@ const autoCompleteJS = new autoComplete({
                 message.innerHTML = `<span class="not_found">Found No Results for "${data.query}"</span>`;
                 // Add message list element to the list
                 list.appendChild(message);
+                const bsCollapse = new bootstrap.Collapse('#pcrequest', {
+                    show: true
+                })
+
             }
         },
         noResults: true,
@@ -110,9 +114,29 @@ const autoCompleteJS = new autoComplete({
                 let latLng = event.detail.selection.value.lat_long;
                 document.getElementById('lat_long').value = latLng;
                 autoCompleteJS.input.value = selection;
-                setMarker(latLng,18);
+                setMarker(latLng, 18);
 
             },
         },
     },
 });
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
